248112a2cd40a4faf9b7d4ce69e78a621b3b0b3d
Maicon <maiconsanchespardinho@gmail.com>
2024-01-06 11:26:00 -0300

    melhorando pergunta para o usuário e loop para confirmar login e senha e adicionando método para o usuário acessar sua nova conta.

--- sistema_de_login_em_java/sistema_de_login.java
+++ sistema_de_login_em_java/sistema_de_login.java
@@ -5,56 +5,81 @@ import java.util.Scanner;
 
 public class sistema_de_login {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in,"UTF-8");
+        Scanner scanner = new Scanner(System.in);
         String jatemconta = "";
-
-        // a string controle serve para que o loop que confirma o login e senha funcione sem nenhuma pessoa cadastrada.
-        String controle = "";
-        String cadastrados = "maicon";
-        String senhacadastrados = "1234";
+        String cadastrados = "";
+        String senhacadastrados = "";
         String login = "";
         String senha = "";
         String confirmarsenha = "";
+        String entrarnaconta = "";
 
         System.out.print("você já possui uma conta conosco? ");
         jatemconta = scanner.nextLine();
 
         // caso o usuário já tenha uma conta.
-        if(jatemconta.equalsIgnoreCase("s") || jatemconta.equalsIgnoreCase("sim")) {
+        if(jatemconta.equalsIgnoreCase("s")) {
             // loop para confirmar o login e senha.
-            while(!login.equals(cadastrados) || !senha.equals(senhacadastrados) || cadastrados.equals(controle) || senhacadastrados.equals(controle)) {
+            while(!login.equals(cadastrados) || !senha.equals(senhacadastrados) || cadastrados.equals("") || senhacadastrados.equals("")) {
                 System.out.print("digite seu login: ");
                 login = scanner.nextLine();
 
                 System.out.print("digite sua senha: ");
                 senha = scanner.nextLine();
 
-                if(!login.equals(cadastrados) || !senha.equals(senhacadastrados)) {
+                // if para ver se o login e senha estão corretos.
+                if(!login.equals(cadastrados) || !senha.equals(senhacadastrados) || login.equals("") || senha.equals("")) {
                     System.out.println("login ou senha incorreta!");
-                } else if(login.equals(cadastrados) && senha.equals(senhacadastrados)) {
+                } else if(login.equals(cadastrados) && senha.equals(senhacadastrados) && !login.equals("") && !senha.equals("")) {
                     System.out.printf("olá %s bem-vindo!",cadastrados);
                 }
                 }  // caso o usuário não tenha uma conta.
-            } else if (jatemconta.equalsIgnoreCase("n") || jatemconta.equalsIgnoreCase("nao") || jatemconta.equals("não")) {
+            } else if (jatemconta.equalsIgnoreCase("n")) {
                 System.out.print("digite seu novo login: ");
                 cadastrados = scanner.nextLine();
 
                 // loop para confirmar a senha.
-                while(!senhacadastrados.equals(confirmarsenha)) {
+                while(!senhacadastrados.equals(confirmarsenha) || senhacadastrados.equals("")) {
                     System.out.print("digite sua nova senha: ");
                     senhacadastrados = scanner.nextLine();
 
                     System.out.print("confirme sua nova senha: ");
                     confirmarsenha = scanner.nextLine();
-
+                    // if para confirmar se a senha está correta ou incorreta. 
                     if(!senhacadastrados.equals(confirmarsenha)) {
                         System.out.println("senha incorreta, por favor digite novamente.");
                     }else {
                         System.out.println("parabéns sua conta foi feita com sucesso!");
+                        System.out.print("gostaria de entrar na sua nova conta S ou N ?");
+                        entrarnaconta = scanner.nextLine();
+                        // if para o usuário acessar sua nova conta.
+                        if(entrarnaconta.equalsIgnoreCase("s")) {
+                            // loop para confirmar o login e senha.
+                            while(!login.equals(cadastrados) || !senha.equals(senhacadastrados) || cadastrados.equals("") || senhacadastrados.equals("")) {
+                                System.out.print("digite seu login: ");
+                                login = scanner.nextLine();
+
+                                System.out.print("digite sua senha: ");
+                                senha = scanner.nextLine();
+
+                                // if para ver se o login e senha estão corretos.
+                                if(!login.equals(cadastrados) || !senha.equals(senhacadastrados) || login.equals("") || senha.equals("")) {
+                                    System.out.println("login ou senha incorreta!");
+                                } else if(login.equals(cadastrados) && senha.equals(senhacadastrados) && !login.equals("") && !senha.equals("")) {
+                                    System.out.printf("olá %s bem-vindo!",cadastrados);
+                                }
+                            } // else if caso o usuário não queira acessar sua nova conta.
+                        }else if(entrarnaconta.equalsIgnoreCase("n")) {
+                            System.out.println("programa finalizado");
+                        }else {
+                            System.out.println("desculpe não entendi");
+                        }
                     }
                 }
+            } else {
+                System.out.println("desculpe não entendi reinicie o programa");
             }
 
         }
- 
-}
+
+    }
